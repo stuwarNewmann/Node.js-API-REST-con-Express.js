35df434c6f05c8ff3206a565442d96d8c32f0ea9
stuwarNewmann <contact.stuwar@gmail.com>
2022-12-18 01:04:04 -0500

    firts server in express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hola mundito");
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Wellcome to my server');
+});
+
+app.listen(port, () => {
+    console.log('listening on port ' + port);
+});
+
